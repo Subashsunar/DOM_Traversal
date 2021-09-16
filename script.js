490bd1010c4bd490d540b0c8b3a7d268117e055e
Subashsunar <subashsunar00@gmail.com>
2021-09-16 03:54:33 -0600

    Traversing a HTML DOM element

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// DOM shaped like an object 
+// DOM (Document Object Model) shaped like an object 
 
 // "this" logs the window object and allowing us bunch of other function
 console.log("This: ");
@@ -14,3 +14,30 @@ console.log(document.documentElement);
 
 // document.head is accessing only head portion of the HTML document.
 console.log(document.head);
+
+// We can access the children of the document by following this line of code.
+console.log(document.body.children[1]);
+
+// We are accessing the 1st child of the ul from the body section of the HTML document
+console.log(document.body.children[1].children[0]);
+
+// To access all the elements we can also use other function that provides by window object.
+// To access first element from ul we can also use getElementById function
+console.log(document.getElementById('first-child-ul'));
+
+// These are some ways we can traversing the DOM. We can do more to this element by accessing it.
+// Like we can change its color, font-size, and more.
+var secondElement = document.getElementById('second-child-ul');
+secondElement.style.color = 'red';
+
+secondElement.innerHTML = "JavaScript";
+
+var btnClick = document.getElementById('btnClick');
+btnClick.addEventListener('click', function() {
+    document.getElementById('third-child-ul').innerHTML = 'Hello';
+});
+
+var btnClick = document.getElementById('btnClick');
+btnClick.addEventListener('click', function() {
+    console.log(`Hello`);
+});
